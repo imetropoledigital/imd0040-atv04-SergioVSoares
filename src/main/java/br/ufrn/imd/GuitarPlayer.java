bb9829995e567458a8c3e3df700eb49fa3b43256
SergioVSoares <51217084+SergioVSoares@users.noreply.github.com>
2022-10-11 09:28:23 -0300

    Create GuitarPlayer.java

---
+++ src/main/java/br/ufrn/imd/GuitarPlayer.java
@@ -0,0 +1,12 @@
+package br.ufrn.imd;
+public class GuitarPlayer extends  Musician {
+    public int howManyYears;
+    public boolean playEletronicGuitar;
+    public boolean playAcousticGuitar;
+    public GuitarPlayer(){
+
+    }
+    public void playGuitar(){
+        System.out.println("Tocando guitarra");
+    }
+}
