fd1d1be75c955aacbe8206e239da80f01f29423a
SergioVSoares <51217084+SergioVSoares@users.noreply.github.com>
2022-10-11 09:28:41 -0300

    Create Person.java

---
+++ src/main/java/br/ufrn/imd/Person.java
@@ -0,0 +1,14 @@
+package br.ufrn.imd;
+public class Person {
+    public String name;
+    public String hometown;
+    public Person(){
+
+    }
+    public String hometown(){
+        return this.hometown;
+    }
+    public String name(){
+        return this.name;
+    }
+}
